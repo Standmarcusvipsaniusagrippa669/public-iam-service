2ecd5da1095f09cc0ef49ad3ad61a84d4aea70fa
iancc-dev <iancardenasc@techvalian.com>
2025-07-05 07:53:52 -0400

    feat: create from method in UserResponse

--- src/main/java/com/valiantech/core/iam/user/dto/UserResponse.java
+++ src/main/java/com/valiantech/core/iam/user/dto/UserResponse.java
@@ -1,5 +1,6 @@
 package com.valiantech.core.iam.user.dto;
 
+import com.valiantech.core.iam.user.model.User;
 import com.valiantech.core.iam.user.model.UserStatus;
 
 import java.time.Instant;
@@ -14,4 +15,18 @@ public record UserResponse(
         Instant lastLoginAt,
         Instant createdAt,
         Instant updatedAt
-) {}
+) {
+    // Método factory
+    public static UserResponse from(User user) {
+        return new UserResponse(
+                user.getId(),
+                user.getFullName(),
+                user.getEmail(),
+                user.getEmailValidated(),
+                user.getStatus() != null ? user.getStatus() : null,
+                user.getLastLoginAt(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
+}
