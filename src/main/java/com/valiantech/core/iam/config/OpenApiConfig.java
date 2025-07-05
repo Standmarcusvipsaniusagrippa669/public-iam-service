04f7e3a92844b50b487ab6a450e8ba8335bd19bc
iancc-dev <iancardenasc@techvalian.com>
2025-07-05 08:17:40 -0400

    doc: add jwt use in OpenApiConfig.java

--- src/main/java/com/valiantech/core/iam/config/OpenApiConfig.java
+++ src/main/java/com/valiantech/core/iam/config/OpenApiConfig.java
@@ -3,7 +3,7 @@ package com.valiantech.core.iam.config;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +12,22 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI iamOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("Valiantech IAM API")
                         .description("API para gestión de identidad y acceso")
                         .version("v1.0.0")
-                        .license(new License().name("Licencia Interna").url("https://valiantech.com")))
+                )
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
                 .externalDocs(new ExternalDocumentation()
                         .description("Documentación adicional")
                         .url("https://valiantech.com/docs"));
