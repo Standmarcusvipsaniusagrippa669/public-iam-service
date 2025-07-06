ea90393e4c4b79ab32d940179d85bd3233e98ddc
iancc-dev <iancardenasc@techvalian.com>
2025-07-06 00:43:42 -0400

    feat: implement specific rate limit

--- src/main/java/com/valiantech/core/iam/auth/controller/AuthController.java
+++ src/main/java/com/valiantech/core/iam/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.valiantech.core.iam.auth.dto.AssociatedCompanies;
 import com.valiantech.core.iam.auth.dto.LoginResponse;
 import com.valiantech.core.iam.auth.dto.TokenRequest;
 import com.valiantech.core.iam.auth.service.AuthService;
+import com.valiantech.core.iam.ratelimit.RateLimit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +21,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
+    @RateLimit(capacity = 5, refill = 10)
     public ResponseEntity<AssociatedCompanies> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.fetchCompanies(request));
     }
 
     @PostMapping("/login-with-company")
+    @RateLimit(capacity = 5, refill = 10)
     public ResponseEntity<LoginResponse> loginWithCompany(@RequestBody TokenRequest request) {
         return ResponseEntity.ok(authService.loginWithCompany(request));
     }
