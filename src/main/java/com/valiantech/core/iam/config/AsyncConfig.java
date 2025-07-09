a477575028113799abeb6a170a914533893500ef
Ian Cárdenas <iancardenasc@techvalian.com>
2025-07-08 22:26:13 -0400

    feat: create AsyncConfig.java

---
+++ src/main/java/com/valiantech/core/iam/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.valiantech.core.iam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
