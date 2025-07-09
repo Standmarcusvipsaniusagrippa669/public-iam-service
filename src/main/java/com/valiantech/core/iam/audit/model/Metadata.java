a5056347e0ccb9ff23a1cacbc230dba834865bb6
Ian Cárdenas <iancardenasc@techvalian.com>
2025-07-09 01:47:46 -0400

    feat: update models audit

--- src/main/java/com/valiantech/core/iam/audit/model/Metadata.java
+++ src/main/java/com/valiantech/core/iam/audit/model/Metadata.java
@@ -1,4 +1,9 @@
 package com.valiantech.core.iam.audit.model;
 
-public record Metadata(String description) {
+
+
+public record Metadata(String description, Object object) {
+    public Metadata(String description) {
+        this(description, null);
+    }
 }
